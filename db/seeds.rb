2813ba80f062233379a6b93b8b1923f5bb60d48b
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-06 20:45:26 +0000

    changed quantities of 'HipsterSocks' and 'ElectricChair' to 0

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,7 +63,7 @@ cat1.products.create!({
   name:  'Hipster Socks',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel4.jpg'),
-  quantity: 8,
+  quantity: 0,
   price: 25.00
 })
 
@@ -120,7 +120,7 @@ cat3.products.create!({
   name:  'Electric Chair',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('furniture2.jpg'),
-  quantity: 2,
+  quantity: 0,
   price: 987.65
 })
 
