f4b847f2bd581d792194681b36a2e5888aac0174
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-07 23:33:05 +0000

    added route for admin/categories

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,14 @@ Rails.application.routes.draw do
     post   :add_item
     post   :remove_item
   end
+  
 
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
