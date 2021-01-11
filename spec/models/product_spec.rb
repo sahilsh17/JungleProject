cbe29e2a1a82c4484745ab50da7ad20b812590b9
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-11 23:29:59 +0000

    added rspec tests to validate products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    it "should validate if product is successfully saved in database" do
+      @category = Category.find_or_create_by(name: "Apparel")
+      @product = @category.products.create(name:"Jacket",price:100, quantity:20)
+      
+      expect(@product.id).to be_present 
+    end
+    it "should display error if product name is not provided" do
+      @category = Category.find_or_create_by(name: "Electronics")
+      @product = @category.products.create(price:500, quantity:1)
+      
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+    it "should display error if product price is not set" do
+      @category = Category.find_or_create_by(name: "Electronics")
+      @product = @category.products.create(name: "PS4", quantity:1)
+      
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+    it "should display error if product quantity is not set" do
+      @category = Category.find_or_create_by(name: "Furniture")
+      @product = @category.products.create(name: "Table", price:430)
+      
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+    it "should validate if the product has category assigned to it" do
+      @category = Category.find_or_create_by(name: "Electronics")
+      @product = @category.products.create(name: "PS4", quantity:1)
+      
+      expect(@product.category.name).to eq("Electronics")
+    end
   end
 end
