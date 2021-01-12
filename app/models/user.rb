c6c549208579af4197be9a2a47a38cf0509e602a
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-12 07:48:17 +0000

    created authenticate_user_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, :length => {:minimum => 6}
   validates :password_confirmation, presence: true
-  validates :email, presence: true
+  validates :email, presence: true,:uniqueness => { :case_sensitive => false }
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-
+  
+  def self.authenticate_with_credentials(email,password)
+    
+    if User.find_by(email: email.downcase.strip)&.authenticate(password)
+      user = User.find_by(email: email.downcase.strip)
+    else
+      user = nil
+    end
+  end
 end
