35e5d2daf96c6c223926274061632402d74d8b13
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-12 03:00:39 +0000

    added validations for email format,password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :password, :length => {:minimum => 6}
   validates :password_confirmation, presence: true
- 
+  validates :email, presence: true
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
 end
