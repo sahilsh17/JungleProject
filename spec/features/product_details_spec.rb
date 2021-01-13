a0ad0e023190e00105d1dcea484caae9d2fafdf1
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-13 01:15:28 +0000

    added a test to check if user can navigate to product-details page after clicking on product

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to products details page", type: :feature, js: true do
+ # SETUP
+ before :each do
+  @category = Category.create! name: 'Apparel'
+
+  10.times do |n|
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+end
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    expect(page).to have_css 'article.product'
+    first('article.product').click_link('Details')
+    expect(page).to have_css 'article.product-detail'
+
+    save_screenshot
+
+  end
+
+end
